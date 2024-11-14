d5e40aeb7ac022a3a89769c84fe66dc7b44b2d8a
bvillarroelr <benjamin.villarroel3@gmail.com>
2024-11-14 10:49:17 -0300

    feat: main.c ahora lee entradas en el formato deseado (primero orden del grafo, luego conexiones por filas de adjMatrix)

--- src/main.c
+++ src/main.c
@@ -3,23 +3,35 @@
 #include "graph.h"
 // Compilar usando gcc main.c graph.c -o main
 
-int main(int argc, char const *argv[])
-{
-	Graph* graph = createGraph(5);
-	if (graph == NULL)
-	{
-		fprintf(stderr,"Error al crear grafo\n");
-		free(graph);
+int main(int argc, char const *argv[]){
+	// Primera linea de entrada, orden del grafo.
+	int orden;
+	scanf("%d", &orden);
+	if (orden <= 0){
+		fprintf(stderr, "Error. Orden del grafo no válido.\n");
+		exit(1);
 	}
-	printGraph(graph);
-	addEdge(graph, 1, 4);
-	printGraph(graph);
-	addEdge(graph, 0, 2);
-	printGraph(graph);
-	delEdge(graph, 0, 2);
-	printGraph(graph);
+	Graph* g = createGraph(orden);
+	// Lectura de conexiones del grafo (se asume que se comienza desde el 1 para el usuario):
+	for (int i = 0; i < orden; i++){
+		printf("%d: ", i+1);
+		for (int j = 0; j < orden; j++){
+			int valor;
+			scanf("%d", &valor);
+			if (valor > orden) {
+				fprintf(stderr,"Valor %d es superior al orden del grafo.\n", valor);
+				exit(1);
+			}
+			if (valor == -1){
+				break;
+			}
+			addEdge(g, i, valor-1);
+		}
+	}
 
-	freeGraph(graph);
+	printf("Matriz de adyacencia del grafo ingresado:\n");
+	printGraph(g);
+
 	return 0;
 }
 
